d975120284ed7912a9ae93a28c2b159f0869c1e4
li-zhong-yuan <66005107+li-zhong-yuan@users.noreply.github.com>
2022-01-28 11:47:11 +0800

    Initial

---
+++ lizy1.type/include/lizy1/type.h
@@ -0,0 +1,278 @@
+#pragma once
+
+
+/*
+Lizy1 - Type
+Compile-time type information
+
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+Copyright (c) 2021-2022 Li Zhongyuan
+SPDX-License-Identifier: MIT
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+#include <concepts>
+#include <utility>
+#include <type_traits>
+namespace lizy1::type {
+}
+
+
+namespace lizy1::type::impl_type {  // this namespace and things defined in it, are for this lib's implementation only
+    using namespace std;
+}
+
+
+/// Gadgets
+namespace lizy1::type {
+
+
+    /// _b, _f & _t (Bool Constant)
+    template<bool b> struct _b :std::bool_constant<b> {};
+    using _f = _b<false>;
+    using _t = _b<true>;
+
+
+    /// _i (Int Constant)
+    template<int i> struct _i :std::integral_constant<int, i> {};
+
+
+    /// _Is (Integer Pack)
+    template<int... is> struct _Is;
+
+
+    /// _m (Match Result)
+    template<bool b> struct _m { static constexpr bool is_matched = b; };
+    using _mf = _m<false>;
+    using _mt = _m<true>;
+
+
+    /// _P (Type Pack)
+    template<class... Ts> struct _P;
+
+
+    /// _T (Type Wrapper)
+    template<class T> struct _T { using type = T; };
+
+
+    /// _void (Empty Class)
+    struct _void {};
+
+
+}
+
+
+/// Condition
+namespace lizy1::type {
+
+
+    /// _a (Non-Cvref Test)
+    template<class T> using _a = _b<std::is_same_v<T, std::remove_cvref_t<T>>>;
+
+
+    namespace impl_type {
+        struct __dummy;
+        template<class T, class = decltype(sizeof(T))> constexpr bool __c(int) { return true; }
+        template<class T, enable_if_t<is_function_v<T>, int> = 0> constexpr bool __c(int) { return true; }
+        template<class T, enable_if_t<is_same_v<T, void>, int> = 0> constexpr bool __c(int) { return false; }
+        template<class T> constexpr bool __c(...) { \
+            static_assert(is_same_v<__dummy, T>, "Require T is complete"); return false; }
+    }
+
+
+    /// _c (Complete Test)
+    //  note static_assert(false) on test fail, except testing the type "void"
+    template<class T> using _c = _b<impl_type::__c<T>(0)>;
+
+
+    /// _false (Deduced False)
+    template<class T> using _false = _b<std::is_same_v<impl_type::__dummy, T>>;
+
+
+    namespace impl_type {
+        template<class T, class P> struct _in :_f {};
+        template<class T, template<class...> class C, class U, class... Us> struct _in<T, C<U, Us...>> :_in<T, C<Us...>> {};
+        template<class T, template<class...> class C,          class... Us> struct _in<T, C<T, Us...>> :_t {};
+    }
+    namespace impl_type {
+        template<int i, int... j> struct _in2 :_f {};
+        template<int i, int j, int... k> struct _in2<i, j, k...> :_in2<i, k...> {};
+        template<int i,        int... k> struct _in2<i, i, k...> :_t {};
+        template<template<int> class C, template<int...> class D, int i, int... j> struct _in<C<i>, D<j...>> :_in2<i, j...> {};
+    }
+
+
+    /// _in (In Test)
+    template<class T, class P> using _in = _b<impl_type::_in<T, P>::value>;
+
+
+    /// _s (Same Test)
+    template<class T, class U> using _s = _b<std::is_same_v<T, U>>;
+
+
+    namespace impl_type {
+        template<class    T , template<class...> class D> struct __tpl              :type::_f {};
+        template<class... Ts, template<class...> class D> struct __tpl<D<Ts...>, D> :type::_t {};
+    }
+
+
+    /// _tpl (Template Test)
+    // Is T an instance of class template D ?
+    template<class T, template<class...> class D> using _tpl = _b<impl_type::__tpl<T, D>::value>;
+
+
+}
+
+
+/// Calcuation
+namespace lizy1::type {
+
+
+    /// _A (Remove Cvref)
+    template<class T> using _A = std::remove_cvref_t<T>;
+
+
+    /// _C (Conditional)
+    template<bool b, class T, class U> using _C = std::conditional_t<b, T, U>;
+
+
+    /// _D (Decay)
+    template<class T> using _D = std::decay_t<T>;
+
+
+    /// _Ur (Remove Ref)
+    template<class T> using _Ur = std::remove_reference_t<T>;
+
+
+    namespace impl_type {
+        template<class T> struct __Urr       :_T<T> {};
+        template<class T> struct __Urr<T &&> :_T<T> {};
+    }
+
+
+    // _Urr (Remove Rvalue Reference)
+    template<class T> using _Urr = typename impl_type::__Urr<T>::type;
+
+
+}
+
+
+/// Pack Calculation
+namespace lizy1::type {
+
+
+    namespace impl_type {
+        template<class P, class Q> struct __M;
+        template<template<class...> class P, class... Ts, template<class...> class Q, class... Us> \
+            struct __M<P<Ts...>, Q<Us...>> { using type = P<Ts..., Us...>; };
+        template<template<int  ...> class P, int  ... is, template<int  ...> class Q, int  ... js> \
+            struct __M<P<is...>, Q<js...>> { using type = P<is..., js...>; };
+    }
+
+
+    /// _M (Merge)
+    template<class P, class Q> using _M = typename impl_type::__M<P, Q>::type;
+
+
+    namespace impl_type {
+        template<class P, class Q = _P<>> struct __R2;
+        template<class... Qs> struct __R2<_P<>, _P<Qs...>> { using type = _P<Qs...>; };
+        template<class P, class... Ps, class... Qs> struct __R2<_P<P, Ps...>, _P<Qs...>> :__R2<_P<Ps...>, _P<P, Qs...>> {};
+    }
+    namespace impl_type {
+        template<class P, class Q = void> struct __R;
+        template<template<class...> class C, class... Ps, class... Qs> \
+            struct __R<C<Ps...>, _P<Qs...>> { using type = C<Qs...>; };
+        template<template<class...> class C, class... Ps>              \
+            struct __R<C<Ps...>, void> :__R<C<Ps...>, typename __R2<_P<Ps...>>::type> {};
+    }
+
+
+    /// _R (Reverse)
+    template<class P> using _R = typename impl_type::__R<P>::type;
+
+
+    namespace impl_type {
+        template<int i, int j, class  R > struct __Range;
+        template<int i,        int... rs> struct __Range<i, i, _Is<rs...>> { using type = _Is<rs...>; };
+        template<int i, int j, int... rs> struct __Range<i, j, _Is<rs...>> :__Range<i + 1, j, _Is<rs..., i>> {};
+    }
+
+
+    /// _Range (Integer Range)
+    template<int i, int j> using _Range = typename impl_type::__Range<i, j, _Is<>>::type;
+
+
+}
+
+
+/// Sfinae
+namespace lizy1::type {
+
+
+    /// _If (Enable If)
+    template<bool b, typename R = int> using _If = std::enable_if_t<b, R>;
+
+
+    /// _Inpl (Enable If Constructible)
+    //  note "Inpl" stands for "inplace"
+    template<class T, class... Ts> using _Inpl = _If<std::is_constructible_v<T, Ts...>>;
+
+
+}
+
+
+/// Concept
+namespace lizy1::type {
+
+
+    /// Bool
+    //  require bool is no throw constructible from T
+    template<class T> concept Bool = requires { { bool(std::declval<T>()) } noexcept; };
+
+
+    /// Complete
+    //  require T is complete
+    //  note static_assert(false) on test fail, except testing the type "void"
+    template<class T> concept Complete = _c<T>::value;
+
+
+    /// NonCvref
+    //  require T is non-cvref
+    template<class T> concept NonCvref = _a<T>::value;
+
+
+    /// Template
+    //  require T is an instance of class template D
+    template<class T, template<class...> class D> concept Template = _tpl<T, D>::value;
+
+
+    /// Unique
+    //  note T is some type like std::unique_ptr<...>
+    template<class T> concept Unique =
+        Complete<T>                                     &&
+        NonCvref<T>                                     &&
+        std::is_nothrow_default_constructible<T>::value &&
+        std::is_nothrow_move_constructible<T>::value    &&
+        std::is_destructible<T>::value;
+
+
+}
---
+++ readme.md
@@ -0,0 +1,205 @@
+# lizy1.type
+
+This library defines some compile-time utilities to query or modify the properties of types.
+
+## Table of Contents
+
+- [Package](#package)
+    - [Interface lizy1.type](#interface-lizy1type)
+- [Introduction & Examples](#introduction--examples)
+    - [Gadgets](#gadgets)
+    - [Condition](#condition)
+    - [Calculation](#calculation)
+    - [Pack Calculation](#pack-calculation)
+    - [Concept](#concept)
+- [Build & Integrate](#build--integrate)
+    - [Build via CMake](#build-via-cmake)
+    - [Integrate via CMake](#integrate-via-cmake)
+
+## Package
+
+**Package Name**: lizy1.type
+
+**Compatibility Mode**: AnyNewerVersion
+
+**Targets List**: lizy1.type
+
+### Interface lizy1.type
+
+**Features**: C++20
+
+**Interface Dependencies**: None
+
+## Introduction & Examples
+
+Utilities defined by this library are divided into serveral sections. The following text lists all these sections and their examples. You can find the examples in [/test_and_example/test_and_example.cpp](https://github.com/li-zhong-yuan/lizy1.type/blob/master/test_and_example/test_and_example.cpp).
+
+First, let's create some types and class templates for our examples,
+
+```cpp
+struct A {};
+struct B {};
+struct C {};                    // A, B, and C are complete types
+struct D;
+struct E;
+struct F;                       // D, E, and F are incomplete types
+template<class T> struct G;
+template<class T> struct H;     // G and H are class templates (only declaration)
+```
+
+### Gadgets
+
+This section defines gadgets. Gadgets are helper classes and class templates that form the basis of this library.
+
+```cpp
+/// _b, _f & _t (bool constant)
+static_assert(!_b<false>::value);               // _b<...> is similar to std::bool_constant<...>
+static_assert( _b<true >::value);
+static_assert(std::is_same_v<_f, _b<false>>);   // _f is _b<false>
+static_assert(std::is_same_v<_t, _b<true >>);   // _t is _b<true>
+/// _i (int constant)
+static_assert(_i<7>::value == 7);               // _i<...> is similar to std::integral_constant<int, ...>
+/// _Is (integer pack)
+using Integers = _Is<1, 2, 6, 7>;               // _Is<...> is similar to std::integer_sequence<int, ...>,
+                                                // but the former is incomplete
+/// _m, _mf & _mt (match result)
+static_assert(!_m<false>::is_matched);          // _m<...> is similar to _b<...>,
+                                                // but used by deduction classes mainly
+static_assert( _m<true >::is_matched);
+static_assert(std::is_same_v<_mf, _m<false>>);  // _mf is _m<false>
+static_assert(std::is_same_v<_mt, _m<true >>);  // _mt is _m<true>
+/// _P (type pack)
+using Types = _P<A, B, C, D, E, F>;             // _P<...> is incomplete
+/// _T (type wrapper)
+static_assert(std::is_same_v<_T<A>::type, A>);  // _T<...> is similar to std::type_identity<...>
+/// _void (empty class)
+_void v;                                        // an empty class (but complete) representing void
+std::make_shared<_void>();                      // ok
+// std::make_shared<void>();                    // won't compile
+```
+
+### Condition
+
+This section defines alias templates for type test. Each of these templates refers to either `_b<true>` or `_b<false>` as the test result.
+
+```cpp
+/// _a (non-cvref test)
+static_assert( _a<A         >::value);
+static_assert(!_a<const A   >::value);
+static_assert(!_a<volatile A>::value);
+static_assert(!_a<A &       >::value);
+/// _c (complete test)
+static_assert( _c<A    >::value);
+static_assert( _c<A &  >::value);
+static_assert( _c<int()>::value);
+// static_assert(!_c<D>::value);    // ok, but testing an incomplete type leads to
+                                    // another static assertion failure inside _c<...>
+static_assert(!_c<void >::value);
+/// _false (deduced false)
+static_assert(!_false<A>::value);
+/// _in (in test)
+static_assert( _in<B, _P<A, B, C, D, E>>::value);
+static_assert(!_in<F, _P<A, B, C, D, E>>::value);
+static_assert( _in<_Is<6>, _Is<1, 2, 6, 7>>::value);
+static_assert(!_in<_Is<5>, _Is<1, 2, 6, 7>>::value);
+/// _s (same test)
+static_assert( _s<A, A>::value);
+static_assert(!_s<A, B>::value);
+/// _tpl (template test)
+static_assert( _tpl<G<A>, G>::value);
+static_assert(!_tpl<G<A>, H>::value);
+```
+
+### Calculation
+
+This section defines alias templates for non-pack calculation. **Non-Pack Calculation** is compile-time type calculation that does not involve parameter packs. Each of these templates defined in this section refers to a type as its calculation result.
+
+```cpp
+/// _A (reomve cvref)
+static_assert(_s<_A<const volatile A &>, A>::value);
+/// _C (conditional)
+static_assert(_s<_C<true , A, B>, A>::value);
+static_assert(_s<_C<false, A, B>, B>::value);
+/// _D (decay)
+static_assert(_s<_D<const volatile A &>, A  >::value);
+static_assert(_s<_D<A[3]              >, A *>::value);
+/// _Ur (remove ref)
+static_assert(_s<_Ur<const volatile A & >, const volatile A>::value);
+static_assert(_s<_Ur<const volatile A &&>, const volatile A>::value);
+/// _Urr (remove rvalue reference)
+static_assert(_s<_Urr<const volatile A & >, const volatile A &>::value);
+static_assert(_s<_Urr<const volatile A &&>, const volatile A  >::value);
+```
+
+### Pack Calculation
+
+This section defines alias templates for pack calculation. **Pack Calculation** is compile-time type calculation that involves parameter packs. Each of these templates defined in this section refers to a type as its calculation result.
+
+```cpp
+/// _M (merge)
+static_assert(_s<
+    _M<_P<A, B, C>, _P<D, E, F>>,
+    _P<A, B, C, D, E, F>
+>::value);
+static_assert(_s<
+    _M<_Is<1, 2, 3>, _Is<7, 8, 9>>,
+    _Is<1, 2, 3, 7, 8, 9>
+>::value);
+/// _R (reverse)
+static_assert(_s<
+    _R<_P<A, B, C, D, E, F>>,
+    _P<F, E, D, C, B, A>
+>::value);
+/// _Range (integer range)
+static_assert(_s<_Range<4, 9>, _Is<4, 5, 6, 7, 8>>::value);
+```
+
+### Concept
+
+This section defines some concepts.
+
+```cpp
+/// Bool
+static_assert( Bool<bool  >);
+static_assert( Bool<int   >);
+static_assert( Bool<void *>);
+static_assert(!Bool<A     >);
+/// Complete
+static_assert( Complete<A    >);
+static_assert( Complete<A &  >);
+static_assert( Complete<int()>);
+// static_assert(!Complete<D>);     // ok, but testing an incomplete type leads to
+                                    // another static assertion failure inside Complete<...>
+static_assert(!Complete<void >);
+/// NonCvref
+static_assert( NonCvref<A         >);
+static_assert(!NonCvref<const A   >);
+static_assert(!NonCvref<volatile A>);
+static_assert(!NonCvref<A &       >);
+/// Template
+static_assert( Template<G<A>, G>);
+static_assert(!Template<G<A>, H>);
+/// Unique
+static_assert( Unique<std::unique_ptr<int>>);
+static_assert( Unique<std::shared_ptr<int>>);
+static_assert(!Unique<std::atomic_int>);
+```
+
+## Build & Integrate
+
+### Build via CMake
+
+[This section is incomplete]
+
+### Integrate via CMake
+
+To use this library from a CMake project, you can locate it directly with `find_package()`, after this library is properly installed and CMake paths are properly set,
+
+```cmake
+# CMakeLists.txt
+find_package("lizy1.type" 0 REQUIRED)
+...
+add_library("foo" ...)
+...
+target_link_libraries("foo" PRIVATE "lizy1.type")
+```
